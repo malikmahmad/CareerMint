adef998a0949452c01447667d3d393ca31f336f9
Malik <malik@example.com>
2026-06-06 12:17:18 +0500

    Fix routing: replace createBrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import ResumeBuilder from "./components/ResumeBuilder/ResumeBuilder";
 import ErrorPage from "./components/Error";
 import "./App.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/builder",
-    element: <ResumeBuilder />,
-  },
-]);
-
 function App() {
   return (
-    <main className="main-container">
-      <RouterProvider router={router} />
-    </main>
+    <HashRouter>
+      <main className="main-container">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/builder" element={<ResumeBuilder />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+    </HashRouter>
   );
 }
 
